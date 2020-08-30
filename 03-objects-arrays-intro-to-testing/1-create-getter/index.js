c3f7d9504d21541d2a63e3ad95314f854f84b9d0
gelserkirchen <gelserkirchen@mail.ru>
2020-08-30 12:01:09 +0300

    решил задачу 3.1

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -3,6 +3,45 @@
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
-export function createGetter(path) {
 
+export function createGetter(path) {
+    const array = path.split(".");
+    let counter = 0;
+
+
+    return function (obj) {
+        let result;
+        findFieldInObject(obj);
+
+        // recursion function
+        function findFieldInObject(obj) {
+          let nameOfFieldInObject = array[counter]
+
+          if (Object.keys(obj).includes(nameOfFieldInObject)) {
+            if (counter === array.length - 1) {
+              result = obj[nameOfFieldInObject];
+            } else {
+              counter++;
+              findFieldInObject(obj[nameOfFieldInObject])
+            }
+          }
+        }
+
+        return result;
+    };
 }
+
+// const product = {
+//   category: {
+//     title: "Goods"
+//   }
+// }
+//
+// const getter = createGetter('category.title');
+//
+// console.log(getter(product)); // Goods
+// let array = ['title']
+//
+// let name = 'category'
+// console.log(product.category[array[0]])
+
