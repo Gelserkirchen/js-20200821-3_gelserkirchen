74b7f04670feb704882e2cfbcc6ba179f448fa9c
gelserkirchen <gelserkirchen@mail.ru>
2020-08-30 20:29:54 +0300

    добавил решение задачи 3.3

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -3,6 +3,18 @@
  * @param {object} obj - the initial object
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
-export function invertObj(obj) {
+ export function invertObj(obj) {
+    if (typeof obj !== 'object') {
+      return undefined
+    }
+    const tmpObj = {};
 
+    for (let [key, value] of Object.entries(obj)) {
+       tmpObj[value] = key;
+    }
+
+    return tmpObj;
 }
+
+
+
